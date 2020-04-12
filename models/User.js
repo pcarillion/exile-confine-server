5cd3333949311563c0bc74ca6256afd1e04abd34
Paul Carillion <pcarillion@yahoo.fr>
2020-04-13 01:38:29 +0200

    first commit

---
+++ models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  email: String,
+  password: String,
+
+});
+
+const userModel = mongoose.model("User", userSchema);
+
+module.exports = userModel;
